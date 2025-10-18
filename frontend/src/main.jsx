dba3f19e22ff4f4288d5fb1b291fcb1611d6662c
david <david@stepbackfarm.com>
2025-10-18 01:10:49 -0400

    more color mode fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
+import './datepicker-overrides.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
